ca13f0083b18974a01e8306ae3ee6168ab1ef75d
Connor <connor.biddle@hotmail.com>
2020-12-31 11:32:25 +0000

    Implemented TimeInputModal validation to prevent inputs that are too long, too large or non-numeric.

--- src/components/utilities/TimeInputModal.jsx
+++ src/components/utilities/TimeInputModal.jsx
@@ -10,8 +10,41 @@ const TimeInputModal = ({ condition, close, seconds }) => {
   const [mins, setMins] = useState(Math.floor(seconds / 60));
   const [secs, setSecs] = useState(seconds % 60);
 
-  const changeMins = e => setMins(e.target.value);
-  const changeSecs = e => setSecs(e.target.value);
+  const changeMins = e => {
+    if (isInvalidChange(e, "mins")) return;
+    setMins(e.target.value);
+  };
+
+  const changeSecs = e => {
+    if (isInvalidChange(e, "secs")) return;
+    setSecs(e.target.value);
+  };
+
+  const isInvalidChange = (e, type) => {
+    if (type === "mins" && e.target.value.length > 3) return true;
+    if (
+      type === "secs" &&
+      (e.target.value.length > 2 || parseInt(e.target.value) > 59)
+    )
+      return true;
+
+    if (
+      isNaN(parseInt(e.nativeEvent.data)) &&
+      e.nativeEvent.inputType !== "deleteContentBackward" &&
+      e.nativeEvent.inputType !== "deleteContentBackward" &&
+      e.nativeEvent.inputType !== "deleteWordBackward" &&
+      e.nativeEvent.inputType !== "deleteWordForward"
+    )
+      return true;
+  };
+
+  const onSubmit = e => {
+    e.preventDefault();
+    const [min, sec] = [parseInt(mins), parseInt(secs)];
+    const totalSeconds = min * 60 + sec;
+    console.log(`Formatted time: ${min}:${sec}`);
+    console.log(`Time in seconds: ${totalSeconds}\n`);
+  };
 
   return (
     <Modal condition={condition} close={close}>
@@ -22,17 +55,9 @@ const TimeInputModal = ({ condition, close, seconds }) => {
           alignSelf="center"
           height="100%"
         >
-          <CustomInput
-            value={mins}
-            onChange={changeMins}
-            // numbersOnly
-          />
+          <CustomInput value={mins} onChange={changeMins} placeholder="M" />
           <Separator>:</Separator>
-          <CustomInput
-            value={secs}
-            onChange={changeSecs}
-            // numbersOnly
-          />
+          <CustomInput value={secs} onChange={changeSecs} placeholder="S" />
         </Flex>
         <Row>
           <Column size={6}>
@@ -41,7 +66,13 @@ const TimeInputModal = ({ condition, close, seconds }) => {
             </Button>
           </Column>
           <Column size={6}>
-            <Button color="success" icon="fas fa-check" block type="button">
+            <Button
+              color="success"
+              icon="fas fa-check"
+              block
+              type="button"
+              onClick={onSubmit}
+            >
               Confirm
             </Button>
           </Column>
